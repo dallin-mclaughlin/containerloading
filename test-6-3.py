8d60334cf020100a0eb31a1627a564a0a5f5ce59
nillad12 <116941834+nillad12@users.noreply.github.com>
2022-11-12 16:16:28 +1300

    Add files via upload

---
+++ test-6-3.py
@@ -0,0 +1,72 @@
+from core.base import Base
+from core.renderer import Renderer
+from core.scene import Scene
+from core.camera import Camera
+from core.mesh import Mesh
+from core.texture import Texture
+from geometry.boxGeometry import BoxGeometry
+from geometry.rectangleGeometry import RectangleGeometry
+from geometry.sphereGeometry import SphereGeometry
+from material.surfaceMaterial import SurfaceMaterial
+from material.textureMaterial import TextureMaterial
+from extras.movementRig import MovementRig
+from extras.postprocessor import Postprocessor
+from effects.tintEffect import TintEffect
+from effects.invertEffect import InvertEffect
+from effects.pixelateEffect import PixelateEffect
+from effects.vignetteEffect import VignetteEffect
+from effects.colorReduceEffect import ColorReduceEffect
+from effects.brightFilterEffect import BrightFilterEffect
+from effects.horizontalBlurEffect import HorizontalBlurEffect
+from effects.verticalBlurEffect import VerticalBlurEffect
+from effects.additiveBlendEffect import AdditiveBlendEffect
+
+screenWidth = 1600
+screenHeight = 1000
+
+#render a basic scene
+class Test(Base):
+
+    def initialize(self):
+        print("Initializing program...")
+        self.renderer = Renderer()
+        self.scene = Scene()
+        self.camera = Camera(aspectRatio=screenWidth/screenHeight)
+
+        self.rig = MovementRig()
+        self.rig.add(self.camera)
+        self.scene.add(self.rig)
+        self.rig.setPosition([0,1,4])
+        skyGeometry = SphereGeometry(radius=50)
+        skyMaterial = TextureMaterial(Texture("images/blue.png"))
+        sky = Mesh(skyGeometry, skyMaterial)
+        self.scene.add(sky)
+        grassGeometry = RectangleGeometry(width=100, height=100)
+        grassMaterial = TextureMaterial(Texture("images/grass.jpg"), {"repeatUV": [50,50]})
+        grass = Mesh(grassGeometry, grassMaterial)
+        grass.rotateX(-3.14/2)
+        self.scene.add(grass)
+
+        sphereGeometry = SphereGeometry()
+        sphereMaterial = TextureMaterial(Texture("images/grid.png"))
+        self.sphere = Mesh(sphereGeometry, sphereMaterial)
+        self.sphere.setPosition([0,1,0])
+        self.scene.add(self.sphere)
+
+        self.postprocessor = Postprocessor(self.renderer, self.scene, self.camera)
+        #self.postprocessor.addEffect(TintEffect(tintColor=[0,1,0]))
+        #self.postprocessor.addEffect(ColorReduceEffect(levels=5))
+        #self.postprocessor.addEffect(PixelateEffect(resolution=[screenWidth,screenHeight]))
+        self.postprocessor.addEffect(BrightFilterEffect(2.4))
+        self.postprocessor.addEffect(HorizontalBlurEffect(textureSize=[800,600], blurRadius=50))
+        self.postprocessor.addEffect(VerticalBlurEffect(textureSize=[800,600], blurRadius=50))
+        mainScene = self.postprocessor.renderTargetList[0].texture
+        self.postprocessor.addEffect(AdditiveBlendEffect(mainScene, originalStrength=2,
+                                                        blendStrength=1))
+
+    def update(self):
+        self.postprocessor.render()
+        self.rig.update(self.input, self.deltaTime)
+
+#instantiate this class and run the program
+Test(screenSize=[screenWidth,screenHeight]).run()
